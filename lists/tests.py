04206503f71a902ec5266807e4463a2868dc00c5
gboned <gboned@cifpfbmoll.eu>
2020-04-02 04:42:43 +0200

    feat: View básica devuelve pequeño HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -10,3 +12,18 @@ class HomePageTest(TestCase):
         found = resolve('/')
         # Comprobamos que "/" (la raíz de la página) encuentra una función llamada home_page
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        # Creamos objeto HttpRequest, que es lo que Django ve cuando el navegador de un
+        # usuario pide una página
+        request = HttpRequest()
+        # Lo pasamos a nuestra vista home_page, que devuelve una respuesta
+        response = home_page(request)
+        # Extraemos el .content de la respuesta, con decode lo convertimos a HTML
+        html = response.content.decode('utf8')
+        # El fichero HTML debe empezar con <html>
+        self.assertTrue(html.startswith('<html>'))
+        # Debe contener el título de la página
+        self.assertIn('<title>To-Do lists</title>', html)
+        # Y debe terminar con </html>
+        self.assertTrue(html.endswith('</html>'))
