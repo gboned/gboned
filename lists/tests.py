dba7b601fccdff7441b53810e6b5d24751f87439
gboned <gboned@cifpfbmoll.eu>
2020-04-02 04:08:27 +0200

    test: Pasar primer unit test y URL mapeada

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,12 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        # resolve es la función que Django usa internamente para resolver la URL
+        # y encontrar cuál función de view debe mapear
+        found = resolve('/')
+        # Comprobamos que "/" (la raíz de la página) encuentra una función llamada home_page
+        self.assertEqual(found.func, home_page)
